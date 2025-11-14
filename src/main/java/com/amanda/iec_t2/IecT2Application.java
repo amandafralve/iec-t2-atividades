bd21cfc9509f8b24b8c9020d1e369e1bd9caed97
Amanda Freitas <160404693+amandafralve@users.noreply.github.com>
2025-11-14 19:18:26 -0300

    Add files via upload

---
+++ src/main/java/com/amanda/iec_t2/IecT2Application.java
@@ -0,0 +1,13 @@
+package com.amanda.iec_t2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IecT2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IecT2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/amanda/iec_t2/IecT2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amanda.iec_t2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IecT2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
